c708e2ac233558989afb135100955c836560ecb1
Kijusu <75220799+MaxenceR26@users.noreply.github.com>
2022-08-01 23:27:30 +0200

    Love PY

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ def sumWithPerfection(num, n):
         return float(integer)
 
 
-data = {"javascript": 17400000, "python": 15700000, "java": 14000000, "c/c++": 11000000, "c#": 10000000, "php": 7900000, "kotlin": 5000000, "visual development tools": 5000000, "swift": 3500000, "go": 3300000, "objective c": 2400000, "rust": 2200000, "ruby": 2100000, "dart": 1800000, "lua": 1400000,}
+data = {"javascript": 17400000, "python": 15700000, "java": 14000000, "c/c++": 11000000, "c#": 10000000, "php": 7900000, "kotlin": 5000000, "visual development tools": 5000000, "swift": 3500000, "go": 3300000, "objective c": 2400000, "rust": 2200000, "ruby": 2100000, "dart": 1800000, "lua": 1400000}
 number = []
 
 for element in data:
